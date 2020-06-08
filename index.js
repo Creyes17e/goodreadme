e54521df3cc0b504dcbd68bb77b82c74164b2e33
Carina Reyes <59748133+Creyes17e@users.noreply.github.com>
2020-06-07 22:24:23 -0500

    created an index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+// Array of questions for user
+const questions = [];
+
+// Function to write README file
+function writeToFile(fileName, data) {}
+
+// Function to initialize program
+function init() {}
+
+// Function call to initialize program
+init();
