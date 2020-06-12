1805d074919caed4d7e99163e8a018ffc839a7c5
Carina Reyes <59748133+Creyes17e@users.noreply.github.com>
2020-06-12 17:50:40 -0500

    after user responds, file creates

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const util = require("util");
+//Generates the README template
+const generateMarkdown = require("./generateMarkdown");
+//Writes to file
+const writeFileAsync = util.promisify(fs.writeFile);
 // Array of questions for user
 const questions = [
   { type: "input", message: "What is your project title?", name: "Title" },
@@ -8,37 +13,76 @@ const questions = [
     message: "Describe your project to users:",
     name: "Description",
   },
+  // {
+  //   type: "input",
+  //   message: "Would you like to add a table of contents?",
+  //   name: "Table of Contents",
+  // },
   {
     type: "input",
     message: "What are the installation steps you took?",
     name: "Installation",
   },
-  { type: "input", message: "Provide instructions for use:", name: "Usage" },
+  { type: "input", message: "Provide instructions for usage:", name: "Usage" },
   {
     type: "input",
     message:
       "Include any links, names of anyone or anything that helped you build this project:",
     name: "Credits",
   },
-  { type: "input", message: "Include your license (if any):", name: "License" },
+  {
+    type: "list",
+    message: "Choose your license:",
+    choices: [
+      "None",
+      "Academic Free License",
+      "Boost Software License",
+      "MIT",
+      "ISC",
+      "Microsoft Public Licence",
+      "The Unlicense",
+    ],
+    name: "License",
+  },
+  {
+    type: "input",
+    message: "Input your github username:",
+    name: "Github.Username",
+  },
+  {
+    type: "input",
+    message: "Input your email address:",
+    name: "Email",
+  },
 ];
-
-// Function to write README file
-function writeToFile(fileName, data) {
-  var fileName = data.name.toLowercase().split(" ").join("") + ".json";
-  fs.writeFile(fileName, JSON.stringify(data, null, "\t"), (err) => {
-    if (err) {
-      return console.log(err);
-    }
-    console.log("Success!");
-  });
+//Prompts questions to User
+function promptUser() {
+  return inquirer.prompt(questions);
 }
+// Function to write README file
+// function writeToFile(data) {
+//   var fileName = data.toLowerCase().split(" ").join("") + ".md";
+//   fs.writeFile(fileName, JSON.stringify(data, null, "\t"), function (err) {
+//     if (err) {
+//       return console.log(err);
+//     }
+//     console.log("Success!");
+//   });
+// }
 
 // Function to initialize program
-function init() {
-  inquirer.prompt(questions).then((data) => {
-    writeToFile(fileName, data);
-  });
+async function init() {
+  try {
+    const userResponse = await promptUser();
+
+    const readMeInfo = generateMarkdown(userResponse);
+
+    await writeFileAsync("Test-README.md", readMeInfo);
+
+    console.log("Successfully wrote to README.md");
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 // Function call to initialize program
