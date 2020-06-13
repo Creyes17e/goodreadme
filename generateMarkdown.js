be480666e0cca5252648156585f6990671ce6a59
Carina Reyes <59748133+Creyes17e@users.noreply.github.com>
2020-06-12 19:17:25 -0500

    README.md

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -30,7 +30,7 @@ ${userResponse.Credits}
 License:${userResponse.License}
 
 ## Tests
-To test if this CLI App is functioning before displaying it in your README.md in the function init(), change the "README.md" to something else, this will create a new file and will display the information that the user has inputted.
+To test if this CLI App is functioning before displaying it in your README.md in the function init(), change the "README.md" to "Test-README.md", this will update the test file and will display the information that the user has inputted.
 
 ## Questions
 If you have any questions my github username is ${userResponse.Username} or you can reach me through email ${userResponse.Email}
